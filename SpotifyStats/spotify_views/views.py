f5dba4c41194c075ef0c6998b91b9973f3dcd819
bradye2 <bradye2@berkeley.edu>
2023-10-13 19:04:01 -0400

    slight change to views

--- SpotifyStats/spotify_views/views.py
+++ SpotifyStats/spotify_views/views.py
@@ -9,7 +9,7 @@ SPOTIPY_CLIENT_ID = ''
 SPOTIPY_CLIENT_SECRET = ''
 SPOTIPY_REDIRECT_URI = ''
 
-sp_oauth = SpotifyOAuth(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI, scope='user-read-currently-playing user-top-read')
+
 # Create your views here.
 def login_page(request):
      return HttpResponse('Login Here!')
@@ -18,6 +18,7 @@ def login_template(request):
     return render(request, 'login.html')
 
 def spotify_login(request):
+    sp_oauth = SpotifyOAuth(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI, scope='user-read-currently-playing user-top-read')
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
 
