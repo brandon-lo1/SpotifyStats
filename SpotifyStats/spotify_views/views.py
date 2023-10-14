f5239e05612cb04cb765e43b769499a205ed5c27
bradye2 <bradye2@berkeley.edu>
2023-10-13 22:01:36 -0400

    made slight edits to front end

--- SpotifyStats/spotify_views/views.py
+++ SpotifyStats/spotify_views/views.py
@@ -5,8 +5,8 @@ from spotipy.oauth2 import SpotifyOAuth
 from django.shortcuts import redirect
 import requests
 
-SPOTIPY_CLIENT_ID = 'dc77fc145bf745d4a57a1c35ff15009b'
-SPOTIPY_CLIENT_SECRET = 'fa41f2bd61804591a46f29845f197363'
+SPOTIPY_CLIENT_ID = ''
+SPOTIPY_CLIENT_SECRET = ''
 SPOTIPY_REDIRECT_URI = 'http://localhost:8000/callback'
 
 # Create your views here.
@@ -17,12 +17,14 @@ def login_template(request):
     return render(request, 'login.html')
 
 def spotify_login(request):
+    
     sp_oauth = SpotifyOAuth(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI, scope='user-read-currently-playing user-top-read')
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
 
 def callback(request):
     sp_oauth = SpotifyOAuth(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI, scope='user-read-currently-playing user-top-read')
+   
     if request.GET.get('code'):
         request.session['token_info'] = sp_oauth.get_access_token(request.GET.get('code'))
 
@@ -33,12 +35,12 @@ def callback(request):
 def top_tracks(request):
     token_info = request.session.get('token_info')
     access_token = token_info['access_token']
-
+    
     # Spotipy client with the token
     sp = spotipy.Spotify(auth = access_token)
 
     # Fetch top tracks
     results = sp.current_user_top_tracks(limit = 10)
     tracks = results['items']
-    print(tracks)
+    print(tracks[])
     return render(request, 'top_tracks.html', {'top_tracks': tracks})
